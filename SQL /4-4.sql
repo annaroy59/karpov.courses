e55e9f64af10b5691b1c98122c99db99988ae29b
annaroy59 <141730866+annaroy59@users.noreply.github.com>
2023-08-18 17:15:51 +0500

    Create 4-4.sql

---
+++ SQL /4-4.sql
@@ -0,0 +1,11 @@
+--Назначьте скидку 20% на все товары из таблицы products и отберите те, цена на которые с учётом скидки превышает 100 рублей.
+--Выведите id товаров, их наименования, прежнюю цену и новую цену с учётом скидки. Колонку со старой ценой назовите old_price, с новой — new_price.
+--Результат должен быть отсортирован по возрастанию id товара.
+--Поля в результирующей таблице: product_id, name, old_price, new_price
+SELECT product_id,
+       name,
+       price as old_price,
+       (price-(price/100)*20) as new_price
+FROM   products
+WHERE  (price-(price/100)*20) > 100
+ORDER BY product_id
